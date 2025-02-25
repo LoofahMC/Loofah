7aa4de143735d5630c50f3bc48ef59b78d7645b9
nelind <nel.n.lindberg@gmail.com>
2025-02-25 18:27:07 +0100

    List plugins in crash report on loofah

---
+++ loofah/src/mixins/java/dk/nelind/loofah/mixin/core/CrashReportMixin_Fabric.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Loofah, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Nelind <https://www.nelind.dk>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dk.nelind.loofah.mixin.core;
+
+import net.minecraft.CrashReport;
+import net.minecraft.SystemReport;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.metadata.PluginMetadata;
+
+/** Copied from {@link org.spongepowered.vanilla.mixin.core.CrashReportMixin_Vanilla} */
+@Mixin(CrashReport.class)
+public abstract class CrashReportMixin_Fabric {
+    @Shadow @Final private SystemReport systemReport;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void fabric$addPluginsToEnvironment(final CallbackInfo ci) {
+        this.systemReport.setDetail("Plugins", () -> {
+            final StringBuilder result = new StringBuilder(64);
+            for (final PluginContainer container : Sponge.pluginManager().plugins()) {
+                final PluginMetadata metadata = container.metadata();
+
+                final String name = metadata.name().orElse(metadata.id());
+                result.append("\n\t\t")
+                    .append(name).append(" (").append(metadata.id()).append(") ").append(metadata.version());
+            }
+            return result.toString();
+        });
+    }
+}
