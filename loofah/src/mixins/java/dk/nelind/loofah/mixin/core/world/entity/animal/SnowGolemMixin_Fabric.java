82625e17cebcbcea39784277257f07903544587f
nelind <nel.n.lindberg@gmail.com>
2025-03-09 23:34:17 +0100

    Add SnowGolemMixin_Fabric

--- loofah/src/mixins/java/dk/nelind/loofah/mixin/core/world/entity/animal/SnowGolemMixin_Fabric.java
+++ loofah/src/mixins/java/dk/nelind/loofah/mixin/core/world/entity/animal/SnowGolemMixin_Fabric.java
@@ -22,13 +22,27 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package dk.nelind.loofah.world.server;
+package dk.nelind.loofah.mixin.core.world.entity.animal;
 
-import net.minecraft.server.MinecraftServer;
-import org.spongepowered.common.world.server.SpongeWorldManager;
 
-public final class FabricWorldManager extends SpongeWorldManager {
-    public FabricWorldManager(final MinecraftServer server) {
-        super(server);
+import net.minecraft.world.entity.animal.SnowGolem;
+import net.minecraft.world.level.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.common.bridge.world.entity.GrieferBridge;
+
+/** Copied from {@link org.spongepowered.vanilla.mixin.core.world.entity.animal.SnowGolemMixin_Vanilla} */
+@Mixin(SnowGolem.class)
+public abstract class SnowGolemMixin_Fabric {
+    @Redirect(
+        method = "aiStep()V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z"
+        )
+    )
+    private boolean impl$checkCanGrief(final GameRules gameRules, final GameRules.Key<GameRules.BooleanValue> key) {
+        return gameRules.getBoolean(key) && ((GrieferBridge) this).bridge$canGrief();
     }
 }
